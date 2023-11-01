7b2ff7a74a60a2565bde168bd8a17587e84b076d
Bohdon Sayre <bohdon@gmail.com>
2023-11-01 13:42:31 -0400

    add hero to home page, add fullbleed and transparent navbar support

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,10 +1,14 @@
+/** Scroll to the top of the page. */
 function goToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-function setupHoverVideos() {
-  const videos = document.querySelectorAll("video.thumb-hover, .thumb-hover video");
+/** Set up videos with .thumb-hover class to play on mouse enter. */
+function initHoverVideos() {
+  const videos = document.querySelectorAll(
+    "video.thumb-hover, .thumb-hover video"
+  );
 
   videos.forEach((video) => {
     video.addEventListener("mouseenter", () => {
@@ -18,6 +22,23 @@ function setupHoverVideos() {
   });
 }
 
+/** Add the .scrolled class to .navbar.is-fixed-top based on window scroll. */
+function initNavScroll() {
+  var nav = document.querySelector(".navbar.is-fixed-top");
+  if (!nav) {
+    return;
+  }
+
+  document.addEventListener("scroll", function (event) {
+    if (window.scrollY > nav.offsetHeight * 0.5) {
+      nav.classList.add("scrolled");
+    } else {
+      nav.classList.remove("scrolled");
+    }
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
-  setupHoverVideos();
+  initHoverVideos();
+  initNavScroll();
 });
